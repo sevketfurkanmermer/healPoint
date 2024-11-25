941710804843c8002e3e7d07e91c171e5f023c97
Furkan Mermer <sevketfurkanmermer@gmail.com>
2024-11-25 21:47:02 +0300

    proje oluşturuldu

---
+++ src/main/java/com/proje/healpoint/starter/HealpointApplication.java
@@ -0,0 +1,13 @@
+package com.proje.healpoint.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proje/healpoint/starter/HealpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proje.healpoint.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
