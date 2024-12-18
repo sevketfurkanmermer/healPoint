544be1a98424e7710b1725a4739e08385f5ba246
Ayberk ÖZ <ayberkooz@hotmail.com>
2024-12-19 01:15:30 +0300

    Ayberk -> Date , localDate e çevrildi. DtoPatientIU da gereksiz getAge fonk sillindi

--- src/main/java/com/proje/healpoint/dto/DtoPatientIU.java
+++ src/main/java/com/proje/healpoint/dto/DtoPatientIU.java
@@ -1,20 +1,9 @@
 package com.proje.healpoint.dto;
 
 import lombok.Data;
-
 import java.time.LocalDate;
-import java.time.Period;
-import java.util.Date;
 
 @Data
-public class DtoPatientIU extends DtoUserIU{
-    private Date birthDate;
-    public String getAge() {
-        if (birthDate == null) {
-            return null;
-        }
-        LocalDate birthLocalDate = new java.sql.Date(birthDate.getTime()).toLocalDate();
-        int age = Period.between(birthLocalDate, LocalDate.now()).getYears();
-        return String.valueOf(age);
-    }
+public class DtoPatientIU extends DtoUserIU {
+    private LocalDate birthDate;
 }
